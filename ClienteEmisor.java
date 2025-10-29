b9c20e826b9b5cab3fa450ef31b33966b1509542
NicolásMarioRomero <nm.romeroc1@uniandes.edu.co>
2025-10-28 21:16:22 -0500

    cliente emisor 1.0 terminado

--- ClienteEmisor.java
+++ ClienteEmisor.java
@@ -8,6 +8,7 @@ public class ClienteEmisor extends Thread{
         this.id = id;
         this.numMensajes = numMensajes;
         this.buzon = buzon;
+        setName("Cliente #"+id);
     }
 
     @Override
@@ -15,14 +16,22 @@ public class ClienteEmisor extends Thread{
         try{
             Mensaje mensajeInicial = new Mensaje(id, "INICIO");
             buzon.depositarMensaje(mensajeInicial);
+            System.out.println(getName()+" ha enviado su mensaje inicial");
 
             for (int i = 0; i <= numMensajes; i++){
                 boolean spam = Math.random() < 0.3;
                 Mensaje m = new Mensaje(id, id+"-"+i, "NORMAL", spam);
                 buzon.depositarMensaje(m);
+                System.out.println(getName() + " enviado");
+
+                Thread.sleep((int)(Math.random()*500)+500);
             }
+
+            Mensaje mensajeFinal = new Mensaje(id, "FINAL");
+            buzon.depositarMensaje(mensajeFinal);
+            System.out.println(getName()+" ha enviado su mensaje final");
         } catch (InterruptedException e){
-            System.err.println("interrumpido");
+            System.err.println(getName()+" interrumpido");
         }
         
     }
